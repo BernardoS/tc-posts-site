ad6095a6880d81c34730c1d5d0fbead24a759aa7
Bernardo Souza <bernardo.sfs27@gmail.com>
2024-11-22 19:54:23 -0300

    Commit Inicial - Criação do projeto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import saibaMaisLogo from './assets/saiba-mais-logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="#">
+          <img src={saibaMaisLogo} className="Logo do projeto" alt="Logo do projeto" />
+        </a>
+      </div>
+      <h1>Fiap - Pos-tech - Fase 3</h1>
+    </>
+  )
+}
+
+export default App
